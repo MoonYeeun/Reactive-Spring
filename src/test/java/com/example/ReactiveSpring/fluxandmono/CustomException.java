0c701f4e2cbe4641c013fcbb8b129550f4c69f24
MoonYeeun <aams82@hanmail.net>
2021-02-17 02:52:10 +0900

    feat(test): flux and mono error handling test

---
+++ src/test/java/com/example/ReactiveSpring/fluxandmono/CustomException.java
@@ -0,0 +1,18 @@
+package com.example.ReactiveSpring.fluxandmono;
+
+public class CustomException extends Throwable {
+    private String message;
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public CustomException(Throwable e) {
+        this.message = e.getMessage();
+    }
+}
